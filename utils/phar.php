5b27f0f511d6cbf7ff769a3059a2c7c93a56d3eb
SignpostMarv <github@signpostmarv.name>
2012-01-24 07:45:20 +0000

    moving PHP files into Aurora sub-directory, adding text and markdown files to phar archive.

--- utils/phar.php
+++ utils/phar.php
@@ -2,6 +2,6 @@
 date_default_timezone_set('Europe/London');
 echo 'Building full phar...',"\n";
 $phar = new Phar('../build/Aurora-Sim.php.phar');
-$phar->buildFromDirectory('../', '/\.php$/');
+$phar->buildFromDirectory('../', '/\.(php|txt|md)$/');
 $phar->compress(Phar::GZ, '.php.phar.gz');
 ?>
