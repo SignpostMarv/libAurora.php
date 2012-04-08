0f9ac56f23f2b5cb31331485ed3f337d9d5841fd
SignpostMarv <github@signpostmarv.name>
2012-04-08 20:31:16 +0100

    removing debug code

--- libAurora/DataManager/pdo.php
+++ libAurora/DataManager/pdo.php
@@ -153,7 +153,6 @@ namespace libAurora\DataManager{
 							$v = (string)$v;
 						break;
 					}
-					var_dump($k, $v, $type);
 					$sth->bindValue($k, $v, $type);
 				}
 			}catch(PDOException $e){
